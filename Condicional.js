2326742e903bdda96a99068f420d6bdd40aa0c38
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-03-12 08:13:05 -0300

    n

---
+++ Condicional.js
@@ -0,0 +1,13 @@
+//Iniciar numero
+let numero = 0
+
+//Fazer a condicao SE ou IF 
+if (numero > 0){
+    //Exiba numero positivo
+    console.log("Esse numero eh positivo");
+}else if(numero < 0){
+    //Exiba numero negativo
+    console.log("Esse numero eh negativo")
+}else{
+    console.log("Esse numero eh 0");
+}
