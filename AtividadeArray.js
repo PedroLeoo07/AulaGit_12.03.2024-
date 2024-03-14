2f031dd04b90320099c63333177657cc92253130
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-03-14 17:07:26 -0300

    first commit

---
+++ AtividadeArray.js
@@ -0,0 +1,10 @@
+let cores = ['cinza', 'roxo', 'vermelho'];
+let carros = ['corsa', 'celta', 'prisma'];
+let jogadores = ['Cristiano Ronaldo', 'Messi', 'Neymar'];
+
+console.log(cores);
+console.log(carros);
+console.log(jogadores);
+
+let mensagem = 'O meu jogador favorito é: ' + jogadores[2];
+console.log(mensagem);
